39774bb36e7ce5fb99ac5613b95eb9cd22dfbc97
k3st <kiddoturtle04@gmail.com>
2024-08-25 22:51:30 +0800

    create homepage for the 3D model

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Currently, two official plugins are available:
 
 npm install react-router-dom
 npm install @react-three/fiber
+npm install @react-three/drei
+npm install @react-spring/three
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className=" w-full h-screen relative">
+      <Canvas
+        className=" w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
