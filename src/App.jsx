834413195f79e4a3b57287b0e6d4d6445a91b620
k3st <kiddoturtle04@gmail.com>
2024-08-25 21:36:59 +0800

    add navigation,

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"Home"} />
+          <Route path="/projects" element={"Home"} />
+          <Route path="/abpit" element={"Home"} />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
