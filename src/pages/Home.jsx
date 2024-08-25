656a1a1e9fd52299cd410212d995cb1402b6f9af
k3st <kiddoturtle04@gmail.com>
2024-08-26 01:21:42 +0800

    include a 3D model at homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition];
+  };
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
   return (
     <section className=" w-full h-screen relative">
       <Canvas
@@ -15,6 +30,7 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+          <Island position={islandPosition} scale={islandScale} />
         </Suspense>
       </Canvas>
     </section>
