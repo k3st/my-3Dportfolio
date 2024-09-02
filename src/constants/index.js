05ac4b114a6a5baa8212aea4ac3c318a108d171e
k3st <kiddoturtle04@gmail.com>
2024-09-03 03:42:52 +0800

    edit Project details

--- src/constants/index.js
+++ src/constants/index.js
@@ -141,49 +141,49 @@ export const projects = [
   {
     iconUrl: pricewise,
     theme: "btn-back-red",
-    name: "Amazon Price Tracker",
+    name: "Project1",
     description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
   {
     iconUrl: threads,
     theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
+    name: "Project2",
     description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
   {
     iconUrl: car,
     theme: "btn-back-blue",
-    name: "Car Finding App",
+    name: "Project3",
     description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
   {
     iconUrl: snapgram,
     theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
+    name: "Project4",
     description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
   {
     iconUrl: estate,
     theme: "btn-back-black",
-    name: "Real-Estate Application",
+    name: "Project5",
     description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
   {
     iconUrl: summiz,
     theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
+    name: "Project6",
     description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis in ullam. At fugit eaque voluptatibus amet doloremque odit ut minima distinctio voluptatem repellat illum quis, voluptates doloribus sit odio?",
+    link: "",
   },
 ];
